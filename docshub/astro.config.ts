a6273a191762cf23429c2131535344b586546896
Ariana <aroar85@gmail.com>
2024-09-11 09:47:41 +0100

    feat: imagen docker para producción + multi

--- docshub/astro.config.ts
+++ docshub/astro.config.ts
@@ -1,44 +1,36 @@
 import { defineConfig } from "astro/config";
 
-// Integrations:
+// Integraciones:
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
-// Markdown Config:
+// Configuración de Markdown:
 import mdx from "@astrojs/mdx";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import remarkGfm from "remark-gfm";
 import { HEADING_LINK_ANCHOR } from "./src/components/ui/prose-headings";
 
+import node from '@astrojs/node';
+
 const vercelDeploy = {
   adapter: vercel(),
-  output: "server",
+  output: "server" as const,
   site: "https://docshub.vercel.app",
 }
 
 const dockerDeploy = {
-  vite: {
-    server:{
-      host: "0.0.0.0",
-      hmr: { clientPort: 4321 },
-      port: 4321, 
-      watch: { usePolling: true }
-    }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 4321,
-  },
+  output: "server" as const,
+  adapter: node({
+    mode: 'standalone',
+  }),
 }
 
 // https://astro.build/config
 export default defineConfig({
-  // ...dockerDeploy,
-  adapter: vercel(),
-  output: "server",
-  site: "https://docshub.vercel.app",
+  ...dockerDeploy, 
+  // ...vercelDeploy,
   integrations: [
     react(),
     tailwind({
