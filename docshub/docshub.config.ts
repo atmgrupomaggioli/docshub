0eb00b1bc48d7315af9f55a2c330435425db952e
Roberto <robertoprz898@gmail.com>
2024-09-27 13:44:45 +0100

    fix: ajusted docker images for latest & multiplatform with Vite environments

--- docshub/docshub.config.ts
+++ docshub/docshub.config.ts
@@ -1,41 +1,11 @@
-type LicenseType =
-  | "MIT"
-  | "GPL-3.0"
-  | "Apache-2.0"
-  | "BSD-3-Clause"
-  | "MPL-2.0"
-  | "LGPL-3.0"
-  | "AGPL-3.0"
-  | "Unlicense"
-  | "CC0-1.0"
-  | "Proprietary";
-
-type RepositoryType =
-  | "github"
-  | "gitlab"
-  | "bitbucket";
-
-interface DocshubRepository {
-  type: RepositoryType;
-  url: string;
-}
-
-interface DocshubConfig {
-  documentationTitle: string;
-  version: string;
-  author?: string;
-  license?: LicenseType;
-  repository?: DocshubRepository
-}
-
 const defaultDocshubConfig: DocshubConfig = {
-  documentationTitle: "Example documentation site",
-  version: "0.0.1",
-  author: "Author's name",
-  license: "Proprietary",
+  documentationTitle: import.meta.env.DOCUMENTATION_TITLE || "Example documentation site 2",
+  version: import.meta.env.VERSION || "0.0.1",
+  author: import.meta.env.AUTHOR || "Author's name",
+  license: import.meta.env.LICENSE || "Proprietary",
   repository: {
-    type: "github",
-    url: "https://github.com/atmgrupomaggioli/docshub"
+    type: import.meta.env.REPOSITORY_TYPE || "github",
+    url: import.meta.env.REPOSITORY_URL || "https://github.com/atmgrupomaggioli/docshub"
   }
 };
 
