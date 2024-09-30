64ee6d518b85d606eca2f5c0d3115c49e62d980f
Roberto <robertoprz898@gmail.com>
2024-09-30 10:34:20 +0100

    refactor: components + ui components

--- docshub/src/components/sidebar/sidebar-collapse.tsx
+++ docshub/src/components/sidebar/sidebar-collapse.tsx
@@ -18,13 +18,15 @@ interface SidebarCollapseProps {
 const SidebarCollapse = (props: SidebarCollapseProps) => {
   return (
     <Sheet>
-      <SheetTrigger className="block rounded-md p-2 transition-colors hover:bg-gray-800 md:hidden">
+      <SheetTrigger className="block rounded-md p-2 transition-colors hover:bg-gray-800 xl:hidden">
         <MenuIcon className="h-6" />
       </SheetTrigger>
       <SheetContent className="flex flex-col p-6 py-[12px] text-sm">
         <SheetHeader className="mb-3 flex w-full items-center justify-between border-b border-gray-800 py-2">
-          <span>🔎 Docshub</span>
-          <SheetClose className="hover:opacity-90">X</SheetClose>
+          <span>Docshub</span>
+          <SheetClose className="hover:opacity-90">
+            aqui icono
+          </SheetClose>
         </SheetHeader>
         <nav className="flex flex-col gap-1">{props.children}</nav>
       </SheetContent>
