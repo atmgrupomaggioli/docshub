55fc47f2295e5e34c0a591cc13bf408586ae0218
pheralb <pablohdez4340@gmail.com>
2024-11-29 15:16:57 +0000

    feat: update UI components for light/dark mode, create custom badge, delete unused utilities & update tailwind-prose settings

--- docshub/src/components/ui/button.tsx
+++ docshub/src/components/ui/button.tsx
@@ -1,25 +1,24 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-
 import { cx } from "@/utils/cx";
 
 const buttonVariants = cva(
-  "inline-flex space-x-3 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 focus-visible:ring-gray-700",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-300 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 dark:focus-visible:ring-gray-700",
   {
     variants: {
       variant: {
         default:
-          "shadow bg-gray-50 text-gray-900 hover:bg-gray-50/90",
+          "bg-gray-900 text-gray-50 shadow hover:bg-gray-900/90 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90",
         destructive:
-          "shadow-sm hover:bg-red-500/90 bg-red-900 text-gray-50 bg-red-900/90",
+          "bg-red-500 text-gray-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-gray-50 dark:hover:bg-red-900/90",
         outline:
-          "border shadow-sm hover:bg-gray-100 hover:text-gray-900 border-gray-800 bg-gray-900 hover:bg-gray-800 hover:text-gray-50",
+          "border border-gray-200 bg-white shadow-sm hover:bg-gray-100 hover:text-gray-900 dark:border-gray-800 dark:bg-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50",
         secondary:
-          "shadow-sm hover:bg-gray-100/80 bg-gray-800 text-gray-50 hover:bg-gray-800/80",
+          "bg-gray-100 text-gray-900 shadow-sm hover:bg-gray-100/80 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-800/80",
         ghost:
-          "hover:bg-gray-100 hover:text-gray-900 hover:bg-gray-800 hover:text-gray-50",
-        link: "text-gray-900 underline-offset-4 hover:underline text-gray-50",
+          "hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50",
+        link: "text-gray-900 underline-offset-4 hover:underline dark:text-gray-50",
       },
       size: {
         default: "h-9 px-4 py-2",
--- docshub/src/components/ui/container-page.ts
+++
@@ -1,3 +0,0 @@
-import { cx } from "@/utils/cx";
-
-export const ContainerPage = cx("container px-3")
