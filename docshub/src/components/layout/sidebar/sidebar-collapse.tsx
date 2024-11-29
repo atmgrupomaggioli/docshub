2c34a3d26562853f4e82e70731980bc23472c7e6
pheralb <pablohdez4340@gmail.com>
2024-11-29 15:19:44 +0000

    feat: upgrade UI layout page + new sidebar + new ToC & use ``getEntry()`` utility to render index page

--- docshub/src/components/layout/sidebar/sidebar-collapse.tsx
+++ docshub/src/components/layout/sidebar/sidebar-collapse.tsx
@@ -5,8 +5,8 @@ import {
   SheetContent,
   SheetHeader,
   SheetTrigger,
-} from "../ui/sheet";
-import MenuIcon from "../icons/menu";
+} from "../../ui/sheet";
+import MenuIcon from "../../icons/menu";
 
 interface SidebarCollapseProps {
   children: ReactNode;
@@ -18,7 +18,10 @@ const SidebarCollapse = (props: SidebarCollapseProps) => {
       <SheetTrigger className="block rounded-md p-2 transition-colors hover:bg-gray-800 xl:hidden">
         <MenuIcon className="h-6" />
       </SheetTrigger>
-      <SheetContent className="flex flex-col p-6 py-[12px] text-sm" side={'left'}>
+      <SheetContent
+        className="flex flex-col p-6 py-[12px] text-sm"
+        side={"left"}
+      >
         <SheetHeader className="mb-3 flex w-full justify-between border-b border-gray-800 py-2">
           <div className="flex items-center gap-2">
             <img
@@ -26,10 +29,9 @@ const SidebarCollapse = (props: SidebarCollapseProps) => {
               src="/logo/docshub_transparent.png"
               alt="Docshub Logo"
             />
-            <span className="font-bold text-xl">Docshub</span>
+            <span className="text-xl font-bold">Docshub</span>
           </div>
-          <SheetClose className="hover:opacity-90">
-          </SheetClose>
+          <SheetClose className="hover:opacity-90"></SheetClose>
         </SheetHeader>
         <nav className="flex flex-col gap-1">{props.children}</nav>
       </SheetContent>
--- docshub/src/components/layout/sidebar/sidebar-content.tsx
+++ docshub/src/components/layout/sidebar/sidebar-content.tsx
@@ -2,6 +2,7 @@ import type { CollectionEntry } from "astro:content";
 import { useState } from "react";
 import { cx } from "@/utils/cx";
 import {
+  BoxIcon,
   CornerDownRightIcon,
   FileIcon,
   FileSearch2Icon,
@@ -14,9 +15,11 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "../ui/accordion";
-import { buttonVariants } from "../ui/button";
+} from "@/components/ui/accordion";
+
 import { convertCategory } from "@/utils/convertCategory";
+import { Input } from "@/components/ui/input";
+import { SidebarFolder, SidebarItemActive } from "../sidebar-item";
 
 interface SidebarContentProps {
   docs: CollectionEntry<"docs">[];
@@ -26,15 +29,20 @@ interface SidebarContentProps {
 
 const SidebarContent = (props: SidebarContentProps) => {
   const [inputSearch, setInputSearch] = useState<string>("");
-  // All docs + custom links:
+
+  // 📦 Ignore documents
+  // Then, add the slug manually:
+  const ignoreDocuments = ["ui"];
+
   const allDocs = [
-    ...props.docs.map((doc) => ({
-      ...doc,
-      slug: doc.slug.startsWith("/") ? doc.slug : `/${doc.slug}`,
-    })),
+    ...props.docs
+      .filter((doc) => !ignoreDocuments.includes(doc.slug))
+      .map((doc) => ({
+        ...doc,
+        slug: doc.slug.startsWith("/") ? doc.slug : `/${doc.slug}`,
+      })),
   ];
 
-  // Filter only the docs that match the search:
   const filteredDocs = allDocs.filter(
     (doc) =>
       doc.data.title.toLowerCase().includes(inputSearch.toLowerCase()) ||
@@ -42,58 +50,67 @@ const SidebarContent = (props: SidebarContentProps) => {
         doc.data.description.toLowerCase().includes(inputSearch.toLowerCase())),
   );
 
-  // Obtain all unique categories:
   const categories = Array.from(
     new Set(filteredDocs.map((doc) => doc.data.category)),
   );
 
-  // Group docs by category:
-  const docsByCategory = categories.map((category) => ({
-    category,
-    docs: filteredDocs.filter((doc) => doc.data.category === category),
-  }));
+  const docsByCategory = categories
+    .map((category) => ({
+      category,
+      docs: filteredDocs.filter((doc) => doc.data.category === category),
+    }))
+    .filter((group) => group.docs.length > 0);
 
   // Document Styles:
-  const docItemClasses = cx(
-    "ml-[10px] truncate border-l border-gray-700 py-2 pl-4 text-gray-400 hover:text-white transition-colors",
-  );
   const iconStroke = 1.5;
 
   return (
-    <div className={cx("flex flex-col space-y-3", props.className)}>
+    <div className={cx("flex flex-col space-y-4", props.className)}>
       <div className="relative w-full text-[12px]">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3">
           <div className="pointer-events-none">
             <SearchIcon
               strokeWidth={iconStroke}
               size={16}
-              className="text-gray-400"
+              className="text-gray-600 dark:text-gray-400"
             />
           </div>
         </div>
-        <input
+        <Input
           type="search"
           value={inputSearch}
           onChange={(e) => setInputSearch(e.target.value)}
           placeholder="Search..."
           autoComplete="off"
-          className="w-full rounded-md border border-gray-800 bg-gray-800/20 py-2 pl-10 pr-3 text-sm text-white focus:outline-none focus:ring-1 focus:ring-gray-700"
+          className={cx(
+            "border-gray-300 pl-[36px] dark:border-gray-700",
+            "focus-visible:ring-gray-400 dark:focus-visible:ring-gray-600",
+          )}
         />
       </div>
-      <nav className="flex w-full flex-col space-y-1 text-sm font-medium">
+      <nav className="flex w-full flex-col text-sm">
         <a
           href="/"
           className={cx(
-            buttonVariants({
-              variant: "ghost",
-            }),
-            "w-full justify-between px-1.5 text-gray-400",
-            props.pathname === "/" && "text-white",
+            SidebarFolder,
+            props.pathname === "/" && SidebarItemActive,
           )}
         >
           <div className="flex items-center space-x-3">
             <HouseIcon strokeWidth={iconStroke} size={16} />
             <span>Introduction</span>
+          </div>
+        </a>
+        <a
+          href="/ui"
+          className={cx(
+            SidebarFolder,
+            props.pathname === "/ui" && SidebarItemActive,
+          )}
+        >
+          <div className="flex items-center space-x-3">
+            <BoxIcon strokeWidth={iconStroke} size={16} />
+            <span>Components</span>
           </div>
         </a>
         {inputSearch ? (
@@ -105,9 +122,9 @@ const SidebarContent = (props: SidebarContentProps) => {
                   href={doc.slug}
                   title={doc.slug}
                   className={cx(
-                    docItemClasses,
+                    SidebarFolder,
                     props.pathname.replace(/\/$/, "") === doc.slug &&
-                      "border-gray-400 text-white",
+                      SidebarItemActive,
                   )}
                 >
                   <div className="flex flex-col">
@@ -141,7 +158,7 @@ const SidebarContent = (props: SidebarContentProps) => {
                   : ""
               }
               className="flex flex-col space-y-2"
-              collapsible
+              collapsible={true}
             >
               <AccordionItem
                 key={category.category}
@@ -149,33 +166,34 @@ const SidebarContent = (props: SidebarContentProps) => {
                 className="border-none"
               >
                 <AccordionTrigger
-                  className={buttonVariants({
-                    variant: "ghost",
-                    className: cx(
-                      "w-full justify-between px-2 text-gray-400 hover:text-white",
-                      category.docs.some(
-                        (doc) => doc.slug === props.pathname.replace(/\/$/, ""),
-                      ) && "text-white",
-                    ),
-                  })}
+                  className={cx(
+                    SidebarFolder,
+                    "justify-between",
+                    category.docs.some(
+                      (doc) => doc.slug === props.pathname.replace(/\/$/, ""),
+                    ) && "font-medium text-gray-900 dark:text-gray-50",
+                  )}
                 >
                   <div className="flex items-center space-x-3">
                     <FolderIcon strokeWidth={iconStroke} size={16} />
                     <span>{convertCategory(category.category)}</span>
                   </div>
                 </AccordionTrigger>
-                <AccordionContent className="flex w-full flex-col">
+                <AccordionContent className="mb-2 flex w-full flex-col">
                   {category.docs.map((doc) => (
                     <a
                       key={doc.slug}
                       href={doc.slug}
                       className={cx(
-                        docItemClasses,
+                        SidebarFolder,
+                        "border-l border-gray-300 dark:border-gray-700",
+                        "ml-[14px]",
                         props.pathname.replace(/\/$/, "") === doc.slug &&
-                          "border-gray-400 text-white",
+                          SidebarItemActive,
                       )}
                     >
-                      {doc.data.sidebarTitle}
+                      <FileIcon strokeWidth={iconStroke} size={16} />
+                      <span>{doc.data.sidebarTitle}</span>
                     </a>
                   ))}
                 </AccordionContent>
