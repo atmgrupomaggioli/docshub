dcadc1de55cef8c7e7af54890eb21c6a250bb7c8
pheralb <pablohdez4340@gmail.com>
2024-11-29 15:20:28 +0000

    feat: prepare theme UI app switch (preview)

---
+++ docshub/src/components/themeToggle.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { MoonIcon, SunIcon } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+type Theme = "theme-light" | "dark" | "system";
+
+const ThemeToggle = () => {
+  const [theme, setThemeState] = useState<Theme>("theme-light");
+
+  useEffect(() => {
+    const isDarkMode = document.documentElement.classList.contains("dark");
+    setThemeState(isDarkMode ? "dark" : "theme-light");
+  }, []);
+
+  useEffect(() => {
+    const isDark =
+      theme === "dark" ||
+      (theme === "system" &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+    document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+  }, [theme]);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          {theme === "dark" && <SunIcon size={22} strokeWidth={1.5} />}
+          {theme === "theme-light" && <MoonIcon size={22} strokeWidth={1.5} />}
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setThemeState("theme-light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setThemeState("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setThemeState("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ThemeToggle;
