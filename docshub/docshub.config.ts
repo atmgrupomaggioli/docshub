3f5029c20b6303dc66b906873bd03de882d2c814
pheralb <pablohdez4340@gmail.com>
2024-12-02 13:15:32 +0000

    feat: add ``logoUrl`` property

--- docshub/docshub.config.ts
+++ docshub/docshub.config.ts
@@ -1,12 +1,17 @@
 const defaultDocshubConfig: DocshubConfig = {
-  documentationTitle: import.meta.env.DOCUMENTATION_TITLE || "Example documentation site",
+  documentationTitle: import.meta.env.DOCUMENTATION_TITLE || "Docshub Demo",
   version: import.meta.env.VERSION || "0.0.1",
   author: import.meta.env.AUTHOR || "Author's name",
   license: import.meta.env.LICENSE || "Proprietary",
+  logoUrl:
+    import.meta.env.LOGO_URL ||
+    "https://github.com/atmgrupomaggioli/docshub/blob/main/docshub/public/logo/docshub_transparent.png?raw=true",
   repository: {
     type: import.meta.env.REPOSITORY_TYPE || "github",
-    url: import.meta.env.REPOSITORY_URL || "https://github.com/atmgrupomaggioli/docshub"
-  }
+    url:
+      import.meta.env.REPOSITORY_URL ||
+      "https://github.com/atmgrupomaggioli/docshub",
+  },
 };
 
 export default defaultDocshubConfig;
