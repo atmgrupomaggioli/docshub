d098b826e5c12a951f1bfe99c494a5f888661d30
pheralb <pablohdez4340@gmail.com>
2024-12-03 12:23:42 +0000

    feat: create ``targetBlank`` & ``addMermaidClassname`` rehype plugins to open external links in a new tab + add ``mermaid`` class to HTML

---
+++ docshub/src/components/mdx/plugins/targetBlank.ts
@@ -0,0 +1,19 @@
+import type { RehypePlugin } from "@astrojs/markdown-remark";
+import type { Element } from "hast";
+
+import { visit } from "unist-util-visit";
+
+export const targetBlank: RehypePlugin = ({ domain = "" } = {}) => {
+  return (tree) => {
+    visit(tree, "element", (e: Element) => {
+      if (
+        e.tagName === "a" &&
+        e.properties?.href &&
+        e.properties.href.toString().startsWith("http") &&
+        !e.properties.href.toString().includes(domain)
+      ) {
+        e.properties!["target"] = "_blank";
+      }
+    });
+  };
+};
