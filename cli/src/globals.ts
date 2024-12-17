6732036af3efe4c3bae2230fd69d8de04787cbc6
Roberto <robertoprz898@gmail.com>
2024-12-17 12:59:51 +0000

    chore: new CLI fetch url download

--- cli/src/globals.ts
+++ cli/src/globals.ts
@@ -1,1 +1,1 @@
-export const dockerComposeUrl = 'https://raw.githubusercontent.com/atmgrupomaggioli/docshub/refs/heads/main/docker/docker-compose.yml';
+export const dockerComposeUrl = 'https://raw.githubusercontent.com/atmgrupomaggioli/docshub/refs/heads/main/resources/docker-compose.yml';
