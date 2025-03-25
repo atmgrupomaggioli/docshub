44a6eb37840fe43b6a389a0a070e9f10b9ff1887
pheralb <pablohdez4340@gmail.com>
2025-03-25 15:03:39 +0000

    feat: create custom ``sortDocs`` utility to order by ``order`` property + ``date`` property

--- docshub/src/components/layout/sidebar/sidebar-content.tsx
+++ docshub/src/components/layout/sidebar/sidebar-content.tsx
@@ -30,7 +30,7 @@ import SearchDocs from "@/components/searchDocs";
 import { SidebarFolder, SidebarItemActive } from "../sidebar-item";
 
 import { convertCategory } from "@/utils/convertCategory";
-import { allDocs } from "@/utils/docs";
+import { sortDocs } from "@/utils/docs";
 
 interface SidebarContentProps {
   docs: CollectionEntry<"docs">[];
@@ -43,37 +43,12 @@ interface SidebarContentProps {
 const ignoreDocuments = ["getting-started"];
 
 const SidebarContent = (props: SidebarContentProps) => {
-  const getDocs = allDocs({
-    ignoreDocuments,
-    docs: props.docs,
-  });
-
-  const categories = Array.from(
-    new Set(getDocs.map((doc) => doc.data.category)),
-  );
-
-  // Group docs by category:
-  const docsByCategory = [
-    {
-      category: null,
-      docs: getDocs
-        .filter((doc) => !doc.data.category)
-        .sort(
-          (a, b) => (a.data.order ?? Infinity) - (b.data.order ?? Infinity),
-        ),
+  const categorizedDocs = sortDocs({
+    getDocs: props.docs,
+    options: {
+      ignoreDocs: ignoreDocuments,
     },
-    ...categories
-      .filter((category) => category)
-      .map((category) => ({
-        category,
-        docs: getDocs
-          .filter((doc) => doc.data.category === category)
-          .sort(
-            (a, b) => (a.data.order ?? Infinity) - (b.data.order ?? Infinity),
-          ),
-      }))
-      .filter((group) => group.docs.length > 0),
-  ];
+  });
 
   const handleGoToDoc = (slug: string) => {
     window.location.href = `${slug}`;
@@ -101,7 +76,7 @@ const SidebarContent = (props: SidebarContentProps) => {
       <SearchDocs>
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
-          {docsByCategory.map((category) => {
+          {categorizedDocs.map((category) => {
             if (!category.category) {
               return category.docs.map((doc) => (
                 <CommandGroup key={doc.id}>
@@ -154,8 +129,8 @@ const SidebarContent = (props: SidebarContentProps) => {
             <span>Introduction</span>
           </div>
         </a>
-        {docsByCategory.length > 0 &&
-          docsByCategory.map((category) => {
+        {categorizedDocs.length > 0 &&
+          categorizedDocs.map((category) => {
             if (!category.category) {
               return category.docs.map((doc) => (
                 <a
